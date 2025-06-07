06c76342d5ba1a4b6176508840e75462c22b139b
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-06-07 14:40:00 +0800

    chore: rename

--- src/client/java/dev/ctrlneo/fairutils/client/FairUtilsClient.java
+++ src/client/java/dev/ctrlneo/fairutils/client/FairUtilsClient.java
@@ -10,14 +10,14 @@ import org.slf4j.LoggerFactory;
 
 public class FairUtilsClient implements ClientModInitializer {
 
-    public static final String CONFIG_FILE = "playerutils.config.json";
+    public static final String CONFIG_FILE = "fairutils.config.json";
 
     public static final ModuleManager moduleManager = new ModuleManager();
 
     public final ListenersModule listenersModule = new ListenersModule();
     public final DamageIndicatorModule damageIndicatorModule = new DamageIndicatorModule();
 
-    public static final Logger LOGGER = LoggerFactory.getLogger("Player Utils");
+    public static final Logger LOGGER = LoggerFactory.getLogger("Fair Utils");
 
     @Override
     public void onInitializeClient() {
